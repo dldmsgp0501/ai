98559b7aac55b2c3ac10d05cf204af1e28b3bdca
dldmsgp0501 <dlwnsdn090123@gmail.com>
2026-04-24 10:36:07 +0900

    Add files via upload

---
+++ 17_mobile_menu/script/script.js
@@ -0,0 +1,12 @@
+// 준비작업(바인딩)
+$(function(){
+    $("button").click(function(){
+        $(".cover").fadeIn(300);
+        $(".mobile-menu").animate({right:0},300);
+    });
+
+    $(".close").click(function(){
+        $(".mobile-menu").animate({"right":"-100%"},300);
+        $(".cover").fadeOut(300);
+    });
+});
